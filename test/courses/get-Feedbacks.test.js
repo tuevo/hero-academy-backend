b520f164b51b86d1f3793bd25d574c924aa133f1
19424007 <19424007@student.hcmus.edu.vn>
2021-02-21 14:53:15 +0700

    Update case get feedbacks

--- test/courses/get-Feedbacks.test.js
+++ test/courses/get-Feedbacks.test.js
@@ -59,6 +59,31 @@ const getFeedbacksTest = () =>
                 done(e);
             }
         });
+
+        it("Feedbacks test :: Course not found", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get("/api/courses/60223b77f7e4d94848a4eee2/feedbacks")
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.NOT_FOUND);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.include('COURSE_NOT_FOUND');;
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
     });
 
 module.exports = getFeedbacksTest;
