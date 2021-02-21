6d64a3f50e20d4e1fbac5e2a3f2289987786d149
19424007 <19424007@student.hcmus.edu.vn>
2021-02-21 14:06:14 +0700

    Update case get feed backs

--- test/courses/getFeedbacks.test.js
+++ test/courses/getFeedbacks.test.js
@@ -34,7 +34,31 @@ const getFeedbacksTest = () =>
                 done(e);
             }
         });
+        it("Feedbacks test :: Get feedbacks successfully with page, limit", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get("/api/courses/60223b77f7e4d94848a4eeec/feedbacks")
+                    .query({ page: 1, limit: 4 })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
 
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.OK);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes(FeedbacksConstant.MESSAGES.GET_FEEDBACKS.GET_FEEDBACKS_SUCCESSFULLY);
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
     });
 
 module.exports = getFeedbacksTest;
