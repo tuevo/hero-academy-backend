f085637c37706fae71b524a27a468f0d9badc76c
19424007 <19424007@student.hcmus.edu.vn>
2021-02-23 01:30:22 +0700

    Update file index.test.js in test/courses folder

--- test/courses/index.test.js
+++ test/courses/index.test.js
@@ -1,9 +1,11 @@
 const getFeedbacksTest = require("./get-Feedbacks.test");
 const getCourseDetail = require("./get-course-detail.test");
 const getLecturerInfoForCoursePage = require("./get-lecturer-info-for-course-page.test");
+const getChaptersTest = require("./get-chapters.test");
 
 module.exports = () => {
     getFeedbacksTest();
     getCourseDetail();
     getLecturerInfoForCoursePage();
+    getChaptersTest();
 };
