748e7020bd40e3efda0a8b3e7daf28b6df30e0cf
Viettran0408 <viettran040898.vt@gmail.com>
2021-02-23 00:48:44 +0700

    fix sendgird

--- utils/send-grid.js
+++ utils/send-grid.js
@@ -2,14 +2,17 @@ const sgMail = require("@sendgrid/mail");
 const log4js = require("log4js");
 const logger = log4js.getLogger("Services");
 
+let from = "";
 if (process.env.NODE_ENV === "production") {
   sgMail.setApiKey(
     "SG.iOfGDKHtTcyTYKUAWtvRTA.RzUYDKGqe3fzTteGPEkkBefCQUME_s3ONNUfyAupD-s"
   );
+  from = "abc040898.vt@gmail.com";
 } else {
   sgMail.setApiKey(
     "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"
   );
+  from = "trantuanviet040898.vt@gmail.com";
 }
 
 const sendConfirmMail = ({ email, fullName, otpCode }) => {
@@ -17,7 +20,7 @@ const sendConfirmMail = ({ email, fullName, otpCode }) => {
   return new Promise(async (res, rej) => {
     try {
       const msg = {
-        from: "trantuanviet040898.vt@gmail.com",
+        from,
         to: email,
         subject: "Online-Academy - Xác nhận đăng kí",
         text: `Xin chào ${fullName}\n\nOnline-Academy xin gửi đến bạn mã xác nhận mail: ${otpCode}.`,
@@ -39,7 +42,7 @@ const sendAuthorizationMail = ({ email, password, fullName }) => {
   return new Promise(async (res, rej) => {
     try {
       const msg = {
-        from: "trantuanviet040898.vt@gmail.com",
+        from,
         to: email,
         subject: "Online-Academy - Cấp quyền giảng viên",
         text: `Xin chào ${fullName}\n\n
