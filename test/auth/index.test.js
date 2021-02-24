f62f432c57aca9361624437e123beb87579dc7e4
Tue Vo <vothaiminhtue0312@gmail.com>
2021-02-24 22:59:19 +0700

    added testing api sending OTP code by email

--- test/auth/index.test.js
+++ test/auth/index.test.js
@@ -1,9 +1,11 @@
 const loginTest = require("./login.test");
 const registerTest = require("./register.test");
 const changePassTest = require("./changePass.test");
+const sendOtpCode = require('./send-otp-code.test');
 
 module.exports = () => {
   loginTest();
+  sendOtpCode();
   //registerTest();
   //changePassTest();
 };
