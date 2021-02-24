9f4eb038696821ff8b36761732bf804082cc51dc
Tue Vo <vothaiminhtue0312@gmail.com>
2021-02-24 23:08:34 +0700

    added testing api confirming account

--- test/auth/index.test.js
+++ test/auth/index.test.js
@@ -2,10 +2,12 @@ const loginTest = require("./login.test");
 const registerTest = require("./register.test");
 const changePassTest = require("./changePass.test");
 const sendOtpCode = require('./send-otp-code.test');
+const confirmAccount = require('./confirm-account.test');
 
 module.exports = () => {
   loginTest();
   sendOtpCode();
+  confirmAccount();
   //registerTest();
   //changePassTest();
 };
