415227f94a5e5428f586b8171abcdfa50339f85e
19424007 <19424007@student.hcmus.edu.vn>
2021-02-25 16:32:40 +0700

    Test access for get courses list registered  test

--- test/students/get-courses-list-registered.test.js
+++ test/students/get-courses-list-registered.test.js
@@ -14,6 +14,152 @@ let tokenOfStudent = null;
 
 const getCoursesListRegistered = async () =>
     describe("Students::get courses list registered test", () => {
+        //before running api must get token
+        beforeEach((done) => {
+            try {
+                //get token of admin
+                chai
+                    .request(server)
+                    .post("/api/auth/login")
+                    .send({
+                        email: "admin2020@gmail.com",
+                        password: "123456789",
+                    })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            tokenOfAdmin = res.body.data.meta.accessToken;
+                        }
+
+                        //get token of lecturer
+                        chai
+                            .request(server)
+                            .post("/api/auth/login")
+                            .send({
+                                email: "lecturer1@gmail.com",
+                                password: "123456789",
+                            })
+                            .end((err, res) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+
+                                if (res) {
+                                    tokenOfLecturer = res.body.data.meta.accessToken;
+                                }
+                            });
+
+                        //get token of student
+                        chai
+                            .request(server)
+                            .post("/api/auth/login")
+                            .send({
+                                email: "student1@gmail.com",
+                                password: "123456789",
+                            })
+                            .end((err, res) => {
+                                if (err) {
+                                    console.log(err);
+                                }
+
+                                if (res) {
+                                    tokenOfStudent = res.body.data.meta.accessToken;
+                                }
+                            });
+
+                        done();
+                    });
+            } catch (e) {
+                done(e);
+            }
+        });
+
+        it("get courses list registered test :: token is not exists", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get(`/api/students/courses/registrations`)
+                    .set({
+                        accessToken: "",
+                    })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.UNAUTHORIZED);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes("INVALID_TOKEN");
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
+
+        it("get courses list registered test :: admin account is not allowed to access", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get(`/api/students/courses/registrations`)
+                    .set({
+                        accessToken: tokenOfAdmin,
+                    })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.UNAUTHORIZED);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes("ACCESS_IS_NOT_ALLOWED");
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
+
+        it("get courses list registered test :: lecturer account is not allowed to access", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get(`/api/students/courses/registrations`)
+                    .set({
+                        accessToken: tokenOfLecturer,
+                    })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.UNAUTHORIZED);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes("ACCESS_IS_NOT_ALLOWED");
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
 
     });
 
