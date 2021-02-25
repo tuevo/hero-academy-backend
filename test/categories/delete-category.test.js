ce7cc4930b601e944315393fb33243a82771f628
Tue Vo <vothaiminhtue0312@gmail.com>
2021-02-25 22:50:26 +0700

    added test case for api deleting category unit test

--- test/categories/delete-category.test.js
+++ test/categories/delete-category.test.js
@@ -37,7 +37,7 @@ const deleteCategory = async () =>
       }
     });
 
-    it("Category should be not found", (done) => {
+    it("Cannot delete non-existent category", (done) => {
       try {
         chai
           .request(server)
@@ -54,6 +54,34 @@ const deleteCategory = async () =>
                 .to.be.an("array")
                 .that.includes(
                   CategoriesConstant.MESSAGES.DELETE_CATEGORY.CATEGORY_NOT_FOUND
+                );
+            }
+
+            done();
+          });
+      } catch (e) {
+        console.error(e);
+        done(e);
+      }
+    });
+
+    it("Cannot delete category having course", (done) => {
+      try {
+        chai
+          .request(server)
+          .delete("/api/categories/60223325f7e4d94848a4eee5")
+          .set('accessToken', accessToken)
+          .end((err, res) => {
+            if (err) {
+              console.log(err);
+            }
+
+            if (res) {
+              expect(res).to.have.status(HttpStatus.BAD_REQUEST);
+              expect(res.body.messages)
+                .to.be.an("array")
+                .that.includes(
+                  CategoriesConstant.MESSAGES.DELETE_CATEGORY.CATEGORY_ALREADY_EXISTS_REGISTERED_COURSE
                 );
             }
 
