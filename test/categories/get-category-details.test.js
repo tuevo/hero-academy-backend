a9b5481ae3a585874a3f10d5837bfa2106f62466
19424007 <19424007@student.hcmus.edu.vn>
2021-03-01 01:06:18 +0700

    Update get category details test

--- test/categories/get-category-details.test.js
+++ test/categories/get-category-details.test.js
@@ -7,7 +7,7 @@ const server = require("../../app");
 chai.use(chaiHttp);
 
 const constants = require('./constants.test');
-const CategoriesConstant = require("../../modules/categories/categories.controller");
+const CategoriesConstant = require("../../modules/categories/categories.constant");
 
 let tokenOfAdmin = null;
 let tokenOfLecturer = null;
@@ -152,6 +152,37 @@ const getCategoryDetails = async () => {
                             expect(res.body.messages)
                                 .to.be.an("array")
                                 .that.includes("ACCESS_IS_NOT_ALLOWED");
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
+
+        it("get category details test :: get category details successfully", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get(`${constants.BASE_URL}/60223325f7e4d94848a4eee6`)
+                    .set({
+                        accessToken: tokenOfAdmin,
+                    })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.OK);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes(
+                                    CategoriesConstant.MESSAGES.GET_CATEGORY_DETAILS
+                                        .GET_CATEGORY_DETAILS_SUCCESSFULLY
+                                );
                         }
 
                         done();
