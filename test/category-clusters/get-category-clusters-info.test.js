b3f97a235136edfd78f91533a502817bff1d02e7
19424007 <19424007@student.hcmus.edu.vn>
2021-03-05 15:05:06 +0700

    Update get category clusters info test

--- test/category-clusters/get-category-clusters-info.test.js
+++ test/category-clusters/get-category-clusters-info.test.js
@@ -13,7 +13,85 @@ let accessToken = null;
 
 const getCategoryClustersInfo = async () =>
     describe("CategoryClusters :: Get category cluster info", () => {
+        beforeEach((done) => {
+            try {
+                chai
+                    .request(server)
+                    .post("/api/auth/login")
+                    .send({
+                        email: "admin2020@gmail.com",
+                        password: "123456789",
+                    })
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
 
+                        if (res) {
+                            accessToken = res.body.data.meta.accessToken;
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                done(e);
+            }
+        });
+
+        it("CategoryClusters :: Get category cluster info with access token", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get(constants.BASE_URL)
+                    .set('accessToken', accessToken)
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.OK);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes(
+                                    categoryClustersConstant.MESSAGES.GET_CATEGORY_CLUSTERS_INFO.GET_CATEGORY_CLUSTERS_INFO_SUCCESSFULLY
+                                );
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
+
+        it("CategoryClusters :: Get category cluster info", (done) => {
+            try {
+                chai
+                    .request(server)
+                    .get(constants.BASE_URL)
+                    .end((err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+
+                        if (res) {
+                            expect(res).to.have.status(HttpStatus.OK);
+                            expect(res.body.messages)
+                                .to.be.an("array")
+                                .that.includes(
+                                    categoryClustersConstant.MESSAGES.GET_CATEGORY_CLUSTERS_INFO.GET_CATEGORY_CLUSTERS_INFO_SUCCESSFULLY
+                                );
+                        }
+
+                        done();
+                    });
+            } catch (e) {
+                console.error(e);
+                done(e);
+            }
+        });
     });
 
 module.exports = getCategoryClustersInfo;
