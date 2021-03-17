32ae7b43f5d994dea9e13013dc962aab67613cb6
Viettran0408 <viettran040898.vt@gmail.com>
2021-03-17 14:13:23 +0700

    add api update user info by admin

--- modules/users/users.route.js
+++ modules/users/users.route.js
@@ -1,38 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router({});
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer();
 
-const UserController = require('./users.controller');
-const ParametersConstant = require('../../constants/parameters.constant');
-const FileTypesConstant = require('../../constants/file-types.constant');
+const UserController = require("./users.controller");
+const ParametersConstant = require("../../constants/parameters.constant");
+const FileTypesConstant = require("../../constants/file-types.constant");
+const RoleConstant = require("../users/users.constant");
 
-const ValidateMiddleware = require('../../middleware/validate.middleware');
-const CheckAccessTokenMiddleware = require('../../middleware/check-access-token.middleware');
-const ValidateFileTypesMiddleware = require('../../middleware/validate-file-types.middleware');
+const ValidateMiddleware = require("../../middleware/validate.middleware");
+const CheckAccessTokenMiddleware = require("../../middleware/check-access-token.middleware");
+const ValidateFileTypesMiddleware = require("../../middleware/validate-file-types.middleware");
+const CheckRoleMiddleware = require("../../middleware/check-role.middleware");
 
 const {
   UpdateUserInfoValidationSchema,
-} = require('./validations/update-user-info.schema');
+} = require("./validations/update-user-info.schema");
+const {
+  UpdateUserInfoByAdminRoleIdValidationSchema,
+} = require("./validations/update-user-info-by-admin-role.schema");
 
 router.get(
-  '/',
+  "/",
   CheckAccessTokenMiddleware({ isRequired: true }),
   UserController.getUserInfo
 );
 router.put(
-  '/',
-  upload.fields([{ name: 'avatar' }]),
+  "/",
+  upload.fields([{ name: "avatar" }]),
   ValidateMiddleware(UpdateUserInfoValidationSchema, [ParametersConstant.BODY]),
   ValidateFileTypesMiddleware([
     {
-      name: 'avatar',
+      name: "avatar",
       fileTypes: [FileTypesConstant.IMAGE],
       isRequired: false,
     },
   ]),
   CheckAccessTokenMiddleware({ isRequired: true }),
   UserController.updateUserInfo
+);
+router.put(
+  "/:userId",
+  ValidateMiddleware(UpdateUserInfoByAdminRoleIdValidationSchema, [
+    ParametersConstant.BODY,
+    ParametersConstant.PARAMS,
+  ]),
+  CheckAccessTokenMiddleware({ isRequired: true }),
+  CheckRoleMiddleware([RoleConstant.ROLE.ADMIN]),
+  UserController.updateUserInfoByAdminRole
 );
 
 module.exports = router;
--- modules/users/validations/update-user-info.schema.js
+++ modules/users/validations/update-user-info.schema.js
@@ -3,7 +3,6 @@ const Joi = require("@hapi/joi");
 const UpdateUserInfoValidationSchema = Joi.object().keys({
   fullName: Joi.string(),
   introduction: Joi.string().allow(null, ""),
-  isBlocked: Joi.boolean()
 });
 
 module.exports = {
