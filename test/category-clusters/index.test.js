43626cecbcbe4819ca31040140ffa2d80ee6d909
Tue Vo <vothaiminhtue0312@gmail.com>
2021-03-18 13:57:48 +0700

    added unit test for api updating category cluster

--- test/category-clusters/index.test.js
+++ test/category-clusters/index.test.js
@@ -1,5 +1,7 @@
 const addCategoryCluster = require('./add-category-cluster.test');
+const updateCategoryCluster = require('./update-category-cluster.test');
 
 module.exports = () => {
   addCategoryCluster();
+  updateCategoryCluster();
 };
